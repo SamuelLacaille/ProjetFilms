922ad48b941b5e6908080d8628891b094149d4d8
Greugreu <fenris.fitz@hotmail.fr>
2019-12-04 15:22:52 +0100

    Inscription back pt2
    ADD formregister.php
    
    register.php
    form treatment end, transfered to formregister.php
    
    formregister.php
    XSS clean done
    form treatment done

--- register.php
+++ register.php
@@ -2,45 +2,35 @@
 include('inc/header.php');
 include 'inc/pdo.php';
 include 'functions/functions.php';
-
-$errors = array();
-$success = false;
-
-if(!empty($_POST['submitted'])) {
-    //XSS
-    $pseudo = clean($_POST['pseudo']);
-    $email = clean($_POST['email']);
-    $password1 = clean($_POST['password1']);
-    $password2 = clean($_POST['password2']);
-
-    //Validation
-    if (empty($pseudo)) {
-
-    }
-
-}
+include 'inc/formregister.php';
 
 ?>
 
 <body>
 <div class="wrap">
     <h2> Inscription </h2>
-    <form>
+    <form method="post">
         <div>
             <label for="pseudo">Pseudo</label>
-            <input type="text" name="pseudo" id="pseudo" value="">
+            <input type="text" name="pseudo" id="pseudo" value="<?php if (!empty($_POST['pseudo'])) {echo $_POST['pseudo'];}
+            ?>">
+            <?php spanErr($errors, 'pseudo'); ?>
         </div>
         <div>
             <label for="email">Email</label>
-            <input type="text" name="email" id="email" value="">
+            <input type="text" name="email" id="email" value="<?php if (!empty($_POST['email'])) {echo $_POST['email'];}
+            ?>">
+            <?php spanErr($errors, 'email'); ?>
         </div>
         <div>
             <label for="password1">Mot de passe</label>
             <input type="password" name="password1" id="password1" value="">
+            <?php spanErr($errors, 'password'); ?>
         </div>
         <div>
             <label for="password2">Confirmation de mot de passe</label>
             <input type="password" name="password2" id="password2" value="">
+            <?php spanErr($errors, 'password'); ?>
         </div>
 
         <input type="submit" name="submitted" value="Inscription">
