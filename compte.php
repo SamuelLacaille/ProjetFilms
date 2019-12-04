c954245cbbe2c4a31d2198334a5cfd44d5c03d80
Etienne Barassin <etiennebarassin@gmail.com>
2019-12-04 12:36:11 +0100

    HTML Page compte

---
+++ compte.php
@@ -0,0 +1,23 @@
+<?php
+include ('inc/header.php');
+
+
+?>
+<body>
+
+    <div class="wrap">
+    <h2>Mon compte</h2>
+        <div class="compte">
+            <p>Email : </p>
+            <p>Pseudo : </p>
+            <p>Mot de passe : </p>
+        </div>
+    </div>
+</body>
+
+
+<?php
+include ('inc/footer.php');
+
+
+?>
