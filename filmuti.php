ab8aec133a9ee66173a62be8ff3f0288a5bab9a6
Etienne Barassin <etiennebarassin@gmail.com>
2019-12-05 15:05:20 +0100

    validation php page login css message d'erreur

--- filmuti.php
+++ filmuti.php
@@ -1,6 +1,33 @@
 <?php
 include('inc/header.php');
+$errors = array();
+$success = false;
+if (!empty($_POST['submitted'])) {
+    $rate = clean($_POST['rate']);
 
+    if (is_numeric(empty($rate))) {
+        $errors['rate'] = 'Donnez une note entre 1 et 5';
+    } elseif (mb_strlen($rate) > 5) {
+        $errors['rate'] = 'Donnez une note entre 1 et 5';
+    } elseif (mb_strlen($rate) < 1) {
+        $errors['rate'] = 'Donnez une note entre 1 et 5';
+    } else {
+        $sql = "SELECT note FROM movie_user WHERE rate = :note";
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':note', $rate, PDO::PARAM_STR);
+        $query->execute();
+        $rating = $query->fetch();
+        if (!empty($rating)) {
+            $errors['rate'] = 'Vous avez deja voté';
+        }
+    }if (count($errors)== 0) {
+        $sql = "INSERT INTO movie_user VALUES (null, null,,null, :note, null,  NOW())";
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':note', $rate, PDO::PARAM_STR);
+        $query->execute();
+        $success = true;
+    }
+}
 ?>
 
 
@@ -14,11 +41,13 @@ include('inc/header.php');
             <img src="">
             <p>Titre</p>
             <p>Date</p>
-            <a href=""> <p>1</p></a>
-            <a href=""> <p>2</p></a>
-            <a href=""> <p>3</p></a>
-            <a href=""> <p>4</p></a>
-            <a href=""> <p>5</p></a>
+            <form action="filmuti.php" method="post">
+                <input type="number"name="rate" id="rate" placeholder="Note">
+                <p class="error"><?php if (!empty($errors['rate'])) {
+                        echo $errors['rate'];
+                    } ?></p>
+                <input type="submit" name="submitted">
+            </form>
             <a href="">Supprimer ce film</a>
         </div>
     </div>
