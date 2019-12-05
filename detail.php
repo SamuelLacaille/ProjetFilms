6e44d4866ffa98cf58a1c96cee7428121baab772
LACAILLE Samuel <samuel76560@outlook.fr>
2019-12-05 16:10:11 +0100

    index.php -> modif des checkbox (en cours)       -> verification de l'existence d'un poster, si il existe, on l'affiche, sinon on affiche un poster spécial detail.php -> affichage de l'image spécial si le poster n'existe pas style.css -> mise en forme des films, 5 par lignes

--- detail.php
+++ detail.php
@@ -3,29 +3,28 @@ include 'inc/pdo.php';
 include 'functions/functions.php';
 
 
-
 // informations du film selectionné + affiche du film
 //voir pagesingle
 //prettyurl -> url ?slug=sleg du film
 
 
-if(!empty($_GET['slug'])) {
+if (!empty($_GET['slug'])) {
 
 
     $slug = $_GET['slug'];
 
-    $sql = "SELECT * FROM movies_full WHERE slug LIKE '".$slug."%'";
+    $sql = "SELECT * FROM movies_full WHERE slug LIKE '" . $slug . "%'";
     $query = $pdo->prepare($sql);
     $query->execute();
     $film = $query->fetchAll();
-   // debug($film);
-   if(!empty($film)){
+    // debug($film);
+    if (!empty($film)) {
 
-    }else{
-        die ('404');
+    } else {
+        header('Location: 404.php');
     }
-}else{
-    die('404');
+} else {
+    header('Location: 404.php');
 }
 
 include 'inc/header.php'; ?>
@@ -33,34 +32,39 @@ include 'inc/header.php'; ?>
 
     <div class="wrap">
         <div class="infoFilm">
-            <h2><?php echo $film[0]['title']; ?></h2>
-            <img src="posters/<?php echo $film[0]['id']; ?>.jpg" alt="affiche du film">
-            <p>Title : <?php echo $film[0]['title']; ?></p>
-            <p> Years : <?php echo $film[0]['year']; ?> </p>
-            <p>Genres : <?php echo $film[0]['genres'] ?></p>
-            <p>Directors : <?php echo $film[0]['directors'] ?></p>
-            <p>Writers : <?php echo $film[0]['writers'] ?></p>
-            <p>Cast : <?php echo $film[0]['cast'] ?></p>
-            <p>Runtime : <?php echo $film[0]['runtime'] ?>min</p>
-            <p>Plot : <?php echo $film[0]['plot'] ?></p>
-            <p>Rating : <?php echo $film[0]['rating'] ?></p>
+            <?php $filename = 'posters/' . $film[0]['id'] . '.jpg';
 
-
-
+            if (file_exists($filename)) { ?>
+                <h2><?php echo $film[0]['title']; ?></h2>
+                <img src="posters/<?php echo $film[0]['id']; ?>.jpg" alt="affiche du film">
+                <p>Title : <?php echo $film[0]['title']; ?></p>
+                <p> Years : <?php echo $film[0]['year']; ?> </p>
+                <p>Genres : <?php echo $film[0]['genres'] ?></p>
+                <p>Directors : <?php echo $film[0]['directors'] ?></p>
+                <p>Writers : <?php echo $film[0]['writers'] ?></p>
+                <p>Cast : <?php echo $film[0]['cast'] ?></p>
+                <p>Runtime : <?php echo $film[0]['runtime'] ?>min</p>
+                <p>Plot : <?php echo $film[0]['plot'] ?></p>
+                <p>Rating : <?php echo $film[0]['rating'] ?></p>
+            <?php } else { ?>
+                <h2><?php echo $film[0]['title']; ?></h2>
+                <img src="assets/img/afficheInconnu.jpg" alt="affiche du film">
+                <p>Title : <?php echo $film[0]['title']; ?></p>
+                <p> Years : <?php echo $film[0]['year']; ?> </p>
+                <p>Genres : <?php echo $film[0]['genres'] ?></p>
+                <p>Directors : <?php echo $film[0]['directors'] ?></p>
+                <p>Writers : <?php echo $film[0]['writers'] ?></p>
+                <p>Cast : <?php echo $film[0]['cast'] ?></p>
+                <p>Runtime : <?php echo $film[0]['runtime'] ?>min</p>
+                <p>Plot : <?php echo $film[0]['plot'] ?></p>
+                <p>Rating : <?php echo $film[0]['rating'] ?></p>
+            <?php } ?>
 
 
         </div>
 
 
-
-
-
-
-
-
     </div>
-
-
 
 
 <?php include 'inc/footer.php'; ?>
--- index.php
+++ index.php
@@ -5,9 +5,7 @@ include 'functions/functions.php';
 $title = 'homepage';
 
 
-
-
-$sql= "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
+$sql = "SELECT * FROM movies_full ORDER BY RAND() LIMIT 100";
 $query = $pdo->prepare($sql);
 $query->execute();
 
@@ -17,154 +15,86 @@ $movies = $query->fetchAll();
 // debug($movies);
 
 
+$sql = "SELECT DISTINCT genres FROM movies_full ";
+$query = $pdo->prepare($sql);
+$query->execute();
 
-
-
-
-
-
-
-
+$genres = $query->fetchAll();
 
 
 include 'inc/header.php';
 //debug($_SESSION);
 ?>
 
-    <h1>Accueil</h1>
+<h1>Accueil</h1>
 
-    <div class="wrap">
-        <div class="tri">
-            <h2>Affiner la recherche</h2>
-            <form class="triage">
-                <p>Par genre : </p>
-                <!-- beaucoup de checkbox ,<input type='checkbox' name="categorie[]">,  $_POST['categorie']=>array() -->
-                <div>
-                    <input type="checkbox" id="drama" name="drama">
+<div class="wrap">
+    <div class="tri">
+        <h2>Affiner la recherche</h2>
+        <form class="triage" method="post">
+            <p>Par genre : </p>
+            <!-- beaucoup de checkbox ,<input type='checkbox' name="categorie[]">,  $_POST['categorie']=>array() -->
+            <div>
+                <ul>
+                    <?php foreach ($genres as $genre) { ?>
+                        <li><input type='checkbox' name='genres[]' value='<?php $genre['genres']; ?>'>
+                            <?php echo $genre['genres']; ?></li>
+                    <?php } ?>
+                </ul>
 
-                    <label for="drama">Drama</label>
-                </div>
+            </div>
 
-                <div>
-                    <input type="checkbox" id="fantasy" name="fantasy">
-                    <label for="fantasy">Fantasy</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="romance" name="romance">
-                    <label for="romance">Romance</label>
-                </div>
 
-                <div>
-                    <input type="checkbox" id="action" name="action">
-                    <label for="action">Action</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="thriller" name="thriller">
-                    <label for="thriller">Thriller</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="aventure" name="aventure">
-
-                    <label for="aventure">Aventure</label>
-                </div>
-
-                <div>
-                    <input type="checkbox" id="comedy" name="comedy">
-                    <label for="comedy">Comedy</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="sciFi" name="sciFi">
-                    <label for="sciFi">Sci-Fi</label>
-                </div>
-
-                <div>
-                    <input type="checkbox" id="family" name="family">
-                    <label for="family">Family</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="horror" name="horror">
-                    <label for="horror">Horror</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="war" name="war">
-                    <label for="war">War</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="crime" name="crime">
-                    <label for="crime">Crime</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="mistery" name="mistery">
-                    <label for="mistery">Mistery</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="music" name="music">
-                    <label for="music">Music</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="biography" name="biography">
-                    <label for="biography">Biography</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="documentary" name="documentary">
-                    <label for="documentary">Documentary</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="history" name="history">
-                    <label for="history">History</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="animation" name="animation">
-                    <label for="animation">Animation</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="sport" name="sport">
-                    <label for="sport">Sport</label>
-                </div>
-                <div>
-                    <input type="checkbox" id="western" name="western">
-                    <label for="western">Western</label>
-                </div>
-                <p>Par décennie : </p>
-                <div>
-                    <input type="number" id="dateFilm" name="dateFilm"
-                           min="1880" max="2020" step="10">
-                </div>
-
-            </form>
+            <p>Par décennie : </p>
+            <div>
+                <input type="number" id="dateFilm" name="dateFilm"
+                       min="1880" max="2020" step="10">
+            </div>
 
             <input type="submit" value="Trier">
-
-
-        </div>
-
-        <section id="films">
-            <h2>Les films</h2>
-           <?php foreach ($movies as $movie){ ?>
-
-
-            <div class="film">
-                <img src="posters/<?php echo $movie['id']; ?>.jpg" alt="affiche du film">
-                <caption>
-                    <ul>
-                        <li><?php echo $movie['title']; ?></li>
-                        <a href="detail.php?slug=<?php echo $movie['slug']; ?>"target="_blank">Plus d'informations</a>
-                    </ul>
-                </caption>
-            </div>
-            <?php }   ?>
-
-
-
-
-                <input type="button" id="plusfilm" name="plusfilm" value="+ de films">
-        </section>
+        </form>
 
 
     </div>
 
+    <a id="films">
+        <h2>Les films</h2>
+        <?php foreach ($movies as $movie) {
+            $filename = 'posters/' . $movie['id'] . '.jpg';
+
+            if (file_exists($filename)) { ?>
+                <div class="film">
+                    <img src="posters/<?php echo $movie['id']; ?>.jpg" alt="affiche du film">
+                    <caption>
+                        <ul>
+                            <li><?php echo $movie['title']; ?></li>
+                            <a href="detail.php?slug=<?php echo $movie['slug']; ?>" target="_blank">Plus
+                                d'informations</a>
+                        </ul>
+                    </caption>
+                </div>
+            <?php } else { ?>
+                <div class="film">
+                    <img src="assets/img/afficheInconnu.jpg" alt="affiche du film" width="300" height="300">
+                    <caption>
+                        <ul>
+                            <li><?php echo $movie['title']; ?></li>
+                            <a href="detail.php?slug=<?php echo $movie['slug']; ?>" target="_blank">Plus
+                                d'informations</a>
+                        </ul>
+                    </caption>
+                </div>
+            <?php } ?>
+        <?php } ?>
 
 
-<?php include 'inc/footer.php';
+        <button><a href="index.php">+ de films</a>
+    </a></button>
+    </section>
 
+
+</div>
+
+
+<?php include 'inc/footer.php'; ?>
 
