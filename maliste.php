4ebccb553ff5a9ff6624a085b1d0604774e515b7
Etienne Barassin <etiennebarassin@gmail.com>
2019-12-11 09:37:00 +0100

    bouton ajouter ses films

--- maliste.php
+++ maliste.php
@@ -1,10 +1,39 @@
 <?php
-session_start();
-include 'inc/pdo.php';
-include 'functions/functions.php';
+include('inc/pdo.php');
 //recup ID user dans SESSION
 //recup ID film $film0id
-
+$errors = array();
+$success = false;
 // insert movie_user ID use, ID film, NOW, é NULL note, modified at
+    if (!empty ($film['id'])) {
+        $id = $film['id'];
+        $sql = "SELECT id FROM movies_full WHERE id = :id";
+        $query = $pdo->prepare($sql);
+        $query->bindValue(':id', $id, PDO::PARAM_INT);
+        $query->execute();
+        $films = $query->fetch();
+        if (!empty($_SESSION['id'])) {
+            $id2 = $_SESSION['id'];
+            $sql = "SELECT id FROM users WHERE id = :id";
+            $query = $pdo->prepare($sql);
+            $query->bindValue(':id', $id2, PDO::PARAM_INT);
+            $query->execute();
+            $users = $query->fetch();
+
+
+        }
+        if (count($errors) == 0) {
+
+
+            $sql = "INSERT INTO movie_user VALUES (null, :user_id, :movie_id, null, NOW(), null )";
+            $query = $pdo->prepare($sql);
+            $query->bindValue(':user_id', $users, PDO::PARAM_STR);
+            $query->bindValue(':movie_id', $films, PDO::PARAM_STR);
+            $query->execute();
+            $success = true;
+        }
+
+
+    }
 
 
